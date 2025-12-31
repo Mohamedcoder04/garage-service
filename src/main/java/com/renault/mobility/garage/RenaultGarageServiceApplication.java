ff736d91dd72cdc55d58e1ed7c8abd332958c82a
maghzer <mohamed.aghzer@capgemini.com>
2025-12-31 12:14:32 +0100

    scaffold Spring Boot app structure

---
+++ src/main/java/com/renault/mobility/garage/RenaultGarageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.renault.mobility.garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenaultGarageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenaultGarageServiceApplication.class, args);
+	}
+
+}
